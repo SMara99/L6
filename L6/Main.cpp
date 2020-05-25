03059a508ea49aefdda953eb5eef55c733a22edc
Maria Şuteu <mara.suteu@gmail.com>
2020-05-25 13:49:36 +0300

    update

--- L6/Main.cpp
+++ L6/Main.cpp
@@ -3,7 +3,7 @@
 int main() {
 	int x;
 	auto controller = Controller();
-	cout << "For Admin enter 1 and for Client enter 2";
+	cout << "For Admin enter 1 and for Client enter 2\n";
 	cin >> x;
 	if (x == 1) { controller.Run_Admin(); }
 	else if (x == 2) { controller.Run_Client(); }
